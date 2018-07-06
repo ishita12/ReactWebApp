c5c4c437a10f6954e54cd0cf5f94e75a45c5fc98
ishita12 <babel.i@husky.neu.edu>
2018-07-06 09:57:22 -0400

    prepare for deploy

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   mongoURI: 'mongodb://ishita12:ishita12@ds119800.mlab.com:19800/ishitadb',
-  secret: 'secret'
+  secretOrKey: 'secret'
 //  database: 'mongodb://127.0.0.1:27017/reactDB'
 };
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  secret: process.env.SECRET_OR_KEY
+  secretOrKey: process.env.SECRET_OR_KEY
 //  database: 'mongodb://127.0.0.1:27017/reactDB'
 };
